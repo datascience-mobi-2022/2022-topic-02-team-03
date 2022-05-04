63a4402407a2e807fc8346a64a991091996d3b7e
PBru314 <id255@stud.uni-heidelberg.de>
2022-05-04 17:03:32 +0200

    New gitignore

--- Data_Cleanup.R
+++ Data_Cleanup.R
@@ -1,8 +1,10 @@
 # load data beforehand
-#tcga_exp <- readRDS("./RNA-SeqAllTumors/tcga_tumor_log2TPM.RDS")
-#tcga_anno <- readRDS("./RNA-SeqAllTumors/tcga_tumor_annotation.RDS")
+library(ggplot2)
+
+tcga_exp <- readRDS("./RNA-SeqAllTumors/tcga_tumor_log2TPM.RDS")
+tcga_anno <- readRDS("./RNA-SeqAllTumors/tcga_tumor_annotation.RDS")
 #tumor_vs_norm <- readRDS("./tumorvsnormal/tcga_tumor_normal_datascience_proj_2022.RDS")
-#genesets <- readRDS("./hallmarkGenes/hallmarks_genesets.rds")
+genesets <- readRDS("./hallmarkGenes/hallmarks_genesets.rds")
 
 tcga_exp_copy <- tcga_exp
 
@@ -17,3 +19,10 @@ for (element in rownames(tcga_exp)) {
 }
 
 rownames(tcga_exp_copy) <- make.names(IDs, unique = TRUE)
+
+tcga_exp_copy_t <- as.data.frame(t(tcga_exp_copy))
+
+ggplot(tcga_exp_copy_t, aes(x=ADAMTS9, y=AKT1, color=tcga_anno$cancer_type_abbreviation))+
+  geom_point()+
+  scale_x_log10()+
+  scale_y_log10()
