c25297af2a6244c9bec270c8de223988dd3d7242
PBru314 <id255@stud.uni-heidelberg.de>
2022-07-07 00:46:18 +0200

    cleaned regression

--- pan_cancer/Regression.R
+++ pan_cancer/Regression.R
@@ -4,10 +4,21 @@ library(rms)
 library(MASS)
 library(DAAG)
 library(pROC)
-library(ComlpexHeatmap)
+library(ComplexHeatmap)
+library(caret)
 
 ##clean and afterwards split dataset into train set and validation set
 cor.mat <- cor(t(all_gsva), method= "spearman")
+cor.heatmap <- ComplexHeatmap::Heatmap(cor.mat)
+
+cor.mat_rm <- cor.mat
+cor.mat_rm[upper.tri(cor.mat_rm)] <- 0
+cor.mat_rm <- as.data.frame(cor.mat_rm)
+
+lowcor_all_gsva <- as.data.frame(all_gsva[-findCorrelation(cor.mat),])
 
 
+
+train_dataset <- lowcor_all_gsva[,1:6819]
+test_dataset <- lowcor_all_gsva[,6820:9741]
 
