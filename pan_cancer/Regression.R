4fd3d6f3862ce7174b41e90429c2eee3f90ce9c5
MarieKlei <marie.kleinert@stud.uni-heidelberg.de>
2022-07-06 17:26:57 +0200

    Regression

---
+++ pan_cancer/Regression.R
@@ -0,0 +1,13 @@
+#logistic regression on tcga_exp to evaluate tumor type form pathway expression data
+## load needed packages
+library(rms)
+library(MASS)
+library(DAAG)
+library(pROC)
+library(ComlpexHeatmap)
+
+##clean and afterwards split dataset into train set and validation set
+cor.mat <- cor(all_gsva, method= "spearman")
+
+
+
